21417a159b41b10764b86770ed3de0b9e7fe1c9f
MMikael18 <mmikael_18@hotmail.com>
2017-06-16 12:01:48 +0300

    remove app

--- app/template/template-parts/nav-main.php
+++
@@ -1,9 +0,0 @@
-<div id="top-menu">
-<nav id="top-menu-content">
-    <?php //wp_list_pages( '&title_li=' ); ?>
-    <?php wp_nav_menu( array(
-		'theme_location' => 'top',
-		'menu_id'        => 'top-menu',
-	) ); ?>
-</nav> 
-</div>
