cfce089598707e139ecc8bfaa04ce1172ad1113a
MMikael18 <mmikael_18@hotmail.com>
2017-07-17 19:59:18 +0300

    twitter card

--- app/template/header.php
+++ app/template/header.php
@@ -7,7 +7,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Code Basics">
     <meta name="author" content="Matti Uusitalo">
-    <?php wp_head();?> 
+    <?php wp_head();?>
+    <?php
+    #twitter cards hack
+    if(is_single() || is_page()) {
+      global $post;
+
+      $twitter_url    = get_permalink();
+      $twitter_title  = get_the_title();
+      $twitter_desc   = strip_tags (strip_shortcodes(substr((string)$post->post_content,0,110)));
+
+      $twitter_thumbs = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), full );
+      $twitter_thumb  = $twitter_thumbs[0];   
+
+      //if(!$twitter_thumb) {
+      //  $twitter_thumb = 'http://www.gravatar.com/avatar/8eb9ee80d39f13cbbad56da88ef3a6ee?rating=PG&size=75';
+      //}
+
+      ?>
+      <meta name="twitter:card" value="summary" />
+      <meta name="twitter:url" value="<?php echo $twitter_url; ?>" />
+      <meta name="twitter:title" value="<?php echo $twitter_title; ?>" />
+      <meta name="twitter:description" value="<?php echo $twitter_desc; ?>" />
+      <meta name="twitter:image" value="<?php echo $twitter_thumb; ?>" />
+      <meta name="twitter:site" value="@MMikael_18" />
+      <meta name="twitter:creator" value="@MMikael_18" />
+
+      <?php
+    }
+    
+    ?>
   </head>
 
   <body>
--- template-parts/post-page.php
+++ template-parts/post-page.php
@@ -1,13 +1,14 @@
 <?php php_file(); ?>
 <article class="post-page">
 	<a href="/" id="home-link">Home</a>
-	<?php if (has_post_thumbnail() ): ?>
-		<div class="post-thumbnail">
-			<?php the_post_thumbnail('large'); ?>
-		</div><!-- .post-thumbnail -->	
-	<?php endif; ?>
+	
 	<?php if(!is_page()) : ?>
 		<h2 class="post-title"><?php the_title(); ?></h2>	
+	<?php endif; ?>
+	<?php if (has_post_thumbnail() ): ?>
+		<div class="post-thumbnail img-look-thum">
+			<?php the_post_thumbnail('large'); ?>
+		</div><!-- .post-thumbnail -->	
 	<?php endif; ?>
  	<?php the_content(); ?>
 	<p class="post-meta"><?php the_date(); ?> by <a href="//twitter.com/MMikael_18"><?php the_author(); ?></a></p>
