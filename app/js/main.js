7a445950641122d19e4cbf49d143734a4419e9bc
MMikael <mmikael_18@hotmail.com>
2019-09-03 20:23:06 +0300

    screens

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,7 @@
-
-
 /* open the post header top to the page */
-
-
 $( document ).ready(function() {
     $( "[data-hamburger]" ).bind( "click", function() {
         $(this).toggleClass( "is-active" );
-        $("[data-hamburger-target]").toggleClass( "is-active" );
-        
+        $("[data-hamburger-target]").toggleClass( "is-active" );        
     });
 });
-
-
